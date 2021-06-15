b90c1d8174dc7998be16511ff1e7db06d3288d58
dudu@ <57783398+18222655271@users.noreply.github.com>
2021-06-15 17:23:00 +0800

    Delete runtime directory

--- runtime/cache/4e/819c837d54a6ed09abc77a8560a66f.php
+++
@@ -1,4 +0,0 @@
-<?php
-//000000000000
- exit();?>
-a:9:{s:8:"sms_send";a:1:{i:0;s:21:"\addons\alisms\Alisms";}s:10:"sms_notice";a:1:{i:0;s:21:"\addons\alisms\Alisms";}s:9:"sms_check";a:1:{i:0;s:21:"\addons\alisms\Alisms";}s:8:"app_init";a:4:{i:0;s:23:"\addons\crontab\Crontab";i:1;s:17:"\addons\kefu\Kefu";i:2;s:21:"\addons\qrcode\Qrcode";i:3;s:21:"\addons\shopro\Shopro";}s:12:"action_begin";a:1:{i:0;s:23:"\addons\geetest\Geetest";}s:11:"config_init";a:2:{i:0;s:23:"\addons\geetest\Geetest";i:1;s:19:"\addons\third\Third";}s:7:"upgrade";a:2:{i:0;s:17:"\addons\kefu\Kefu";i:1;s:21:"\addons\shopro\Shopro";}s:11:"leesignhook";a:1:{i:0;s:23:"\addons\leesign\Leesign";}s:8:"testhook";a:1:{i:0;s:23:"\addons\ykquest\Ykquest";}}
